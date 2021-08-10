0560a0751e6df62bea1d346e5725af7445784ea0
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-10 20:20:49 -0300

    CTD

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
+// Get all data
+var info = axios.get('https://sheetdb.io/api/v1/mp3zax2x0z3b3')
+.then( response => {
+    return response.data;
+});
+
+
+for (let i = 0; i < info.length; i++) {
+    // Consumption calculation
+    var kmPercorridos = (info[i].odometer - info[i-1].odometer);
+    var consumo = (kmPercorridos / info[i].liters)
+
+    if(info.length <= 1){
+        alert('This is your first refueling!')
+    } else {
+        document.getElementById('consumption') = consumo;
+        fuelChart.data.labels.push(i+1)
+        fuelChart.data.datasets[0].data.push(consumo)
+    }
+}
+
+
 //POST data into Google Sheet
 
 var form = document.getElementById("sheetdb-form");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
   fetch(form.action, {
-    method: "POST",
-    body: new FormData(document.getElementById("sheetdb-form")),
-  })
+      method: "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+    })
     .then((response) => response.json())
     .then((html) => {
-      alert("Nice!");
-      window.location.href = "./index.html";
+        alert("Nice!");
+        window.location.href = "./index.html";
     });
 });
 
+
+
+// Create the chart
+    
+    let chart = document.getElementById('fuel-chart').getContext('2d')
+    
+    let fuelChart = new Chart(chart, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Liters',
+                data: []
+            }]
+        },
+        options: {}
+    })
